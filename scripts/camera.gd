d40fa0b17d13f62d112a722165d157fda3fed4cf
khairul169 <khairulcyber4rt@gmail.com>
2016-06-28 06:08:37 +0700

    Update camera

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,9 +1,11 @@
 extends Camera
 
+var pitch;
 var yaw;
 var dist;
 
 func _ready():
+	pitch = 20.0;
 	yaw = 0.0;
 	dist = 8.0;
 	
@@ -14,7 +16,11 @@ func _fixed_process(delta):
 		return;
 	
 	var player = get_node("../player");
-	var pos = get_global_transform().origin.linear_interpolate(player.get_global_transform().origin+Vector3(dist*sin(deg2rad(yaw)), 3, dist*cos(deg2rad(yaw))), 5*delta);
+	var p = Vector3();
+	p.x = dist*sin(deg2rad(yaw))*cos(deg2rad(pitch));
+	p.y = dist*sin(deg2rad(pitch));
+	p.z = dist*cos(deg2rad(yaw))*cos(deg2rad(pitch));
+	var pos = get_global_transform().origin.linear_interpolate(player.get_global_transform().origin+p, 5*delta);
 	var target = player.get_global_transform().origin;
 	
 	look_at_from_pos(pos, target, Vector3(0,1,0));
